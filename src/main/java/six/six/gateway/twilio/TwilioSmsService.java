339a3c1c8b326e8b924cf369d6d546b8752e5c14
pverkest <peter.verkest@unifly.aero>
2021-03-05 02:44:23 +0100

    CAN-2466 MFA - Add a bit of error handling for Twilio

--- src/main/java/six/six/gateway/twilio/TwilioSmsService.java
+++ src/main/java/six/six/gateway/twilio/TwilioSmsService.java
@@ -2,10 +2,14 @@ package six.six.gateway.twilio;
 
 import com.twilio.Twilio;
 import com.twilio.rest.api.v2010.account.Message;
+import com.twilio.rest.api.v2010.account.Message.Status;
 import com.twilio.type.PhoneNumber;
+import org.jboss.logging.Logger;
 import six.six.gateway.SMSService;
+import six.six.keycloak.authenticator.KeycloakSmsAuthenticatorUtil;
 
 public class TwilioSmsService implements SMSService {
+  private static Logger logger = Logger.getLogger(TwilioSmsService.class);
 
   private final PhoneNumber twilioFromPhoneNumber;
 
@@ -17,7 +21,12 @@ public class TwilioSmsService implements SMSService {
   public boolean send(String phoneNumber, String message, String login, String pw) {
     Twilio.init(login, pw);
 
-    Message.creator(new PhoneNumber(phoneNumber), twilioFromPhoneNumber, message).create();
-    return true;
+    Message sms = Message.creator(new PhoneNumber(phoneNumber), twilioFromPhoneNumber, message).create();
+    if (sms.getErrorCode() != null) {
+      logger.warn("Twilio can't send the SMS. It returns: " + sms);
+      return false;
+    } else {
+      return true;
+    }
   }
 }
