517823867611e7c18de1f46abb0373d90ce69fc3
kacper <kacper.kosciankowski@unifly.aero>
2026-09-03 17:51:38 +0200

    Fix JUnit test discovery
    UP-12429

--- src/test/java/six/six/gateway/twilio/TwilioRestClientFactoryTest.java
+++ src/test/java/six/six/gateway/twilio/TwilioRestClientFactoryTest.java
@@ -1,7 +1,7 @@
 package six.six.gateway.twilio;
 
-import static org.junit.Assert.assertTrue;
 import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import com.twilio.Twilio;
 import com.twilio.http.NetworkHttpClient;
